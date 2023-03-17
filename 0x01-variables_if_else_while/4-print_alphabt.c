f856b57c246e9596ced02a489b6fe04229711ffd
sanotogii <nasaereh@gmail.com>
2023-03-17 13:12:39 -0700

    4-task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ **/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+		putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
