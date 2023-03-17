2294827d4961483f32da0088e13475caaad9c8a0
sanotogii <nasaereh@gmail.com>
2023-03-17 13:20:24 -0700

    5-task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * printing all single nmbrs of base 10
+ * Return: always 0
+ **/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+
+
+}
