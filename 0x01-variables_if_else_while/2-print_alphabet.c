b734eefeb1c53b995c57dcd929d9ce0d4b2b2133
sanotogii <nasaereh@gmail.com>
2023-03-17 12:36:56 -0700

    2-task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ **/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
