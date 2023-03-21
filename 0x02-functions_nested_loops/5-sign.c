8d5ae9cf4b35f6d33f33d136f3194efb4382a0c9
sanotogii <nasaereh@gmail.com>
2023-03-21 13:00:59 -0700

    5-task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - main function
+ * @n: the arg
+ * Return: three returnes
+ **/
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
