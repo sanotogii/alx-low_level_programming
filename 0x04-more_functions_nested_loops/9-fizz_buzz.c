1f286851a9e03bc0299f0cd24c60b7dc321b69fd
sanotogii <nasaereh@gmail.com>
2023-03-23 11:07:59 -0700

    9-task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i < 100)
+	{
+		if ((i % 3 == 0) && (i % 5 != 0))
+			printf("Fizz ");
+		else if ((i % 5 == 0) && (i % 3 != 0))
+			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	printf("Buzz \n");
+	return (0);
+
+}
