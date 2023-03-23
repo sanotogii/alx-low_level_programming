abb1656c4411673d9ca4a5537adbb270578463da
sanotogii <nasaereh@gmail.com>
2023-03-23 09:09:07 -0700

    ?

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ **/
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+}
