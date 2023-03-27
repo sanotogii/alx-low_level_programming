f4964edce3aead81ad0d5d2e1cbe43cd20035d28
sanotogii <nasaereh@gmail.com>
2023-03-27 10:00:21 -0700

    3-task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - main function
+ * @str: arg
+ * Return: void
+ **/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		*str++;
+	}
+	_putchar('\n');
+}
