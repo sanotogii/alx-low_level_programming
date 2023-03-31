bb6dba50d1d6249584396366a11538b542bf7cd2
sanotogii <nasaereh@gmail.com>
2023-03-30 18:58:55 -0700

    5-task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - the main function
+ * @n: array
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
+
