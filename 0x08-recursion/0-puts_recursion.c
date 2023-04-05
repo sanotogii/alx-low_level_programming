3cb830aad5b1ed0792f7faf8581db7e058ac0d2d
sanotogii <nasaereh@gmail.com>
2023-04-05 15:56:15 -0700

    0-task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - the main function
+ * @s: arg
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+
+}
+
