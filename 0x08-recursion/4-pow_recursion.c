52fb52f39856aceb8c0feda4256526e5e8a6f9b1
sanotogii <nasaereh@gmail.com>
2023-04-05 17:35:13 -0700

    4-task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * _pow_recursion - function that returns x^y
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+int _putchar(char);
 void _puts_recursion(char *s);
-int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,6 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-
+int wildcmp(char *s1, char *s2);
 
 #endif
