0b7869c7532b236cb5a990a35c3184e840b37275
sanotogii <nasaereh@gmail.com>
2023-04-09 13:01:02 -0700

    1-task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: 0
+ **/
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
