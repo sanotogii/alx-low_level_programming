7c151f36bb81fe9c7a45f591deb5dc2f0a62f8ac
sanotogii <nasaereh@gmail.com>
2023-04-11 11:02:08 -0700

    fic

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,13 +17,11 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	
 	len = 0;
 
 	while (str[len] != '\0')
 		len++;
-	
-	arr = malloc(sizeof(char)*(len + 1));
+	arr = malloc(sizeof(char) * (len + 1));
 
 	if (arr == NULL)
 		return (NULL);
