d6b0070f95720260018cf99918c26a114a1fda34
sanotogii <nasaereh@gmail.com>
2023-04-11 19:39:15 -0700

    5-task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - main
+ * @ac: arg number
+ * @av: arr
+ * Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	int total_len, i, pos;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	total_len = 0;
+
+	for (i = 0; i < ac; i++)
+		total_len += strlen(av[i]) + 1;
+
+	str = malloc(total_len);
+
+	if (str == NULL)
+		return (NULL);
+
+	pos = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(str + pos, av[i]);
+		pos += strlen(av[i]);
+		str[pos++] = '\n';
+	}
+
+	str[pos] = '\0';
+	return (str);
+}
