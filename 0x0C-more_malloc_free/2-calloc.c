ec14f563d029d8c75e567bb129d192b6ac8751b3
sanotogii <nasaereh@gmail.com>
2023-04-13 15:04:03 -0700

    g

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,6 +33,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr = malloc(nmemb * size);
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	if (ptr == NULL)
 		return (NULL);
 
