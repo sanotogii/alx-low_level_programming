9780a00d34670affab61a7df4d3ba3e2b1b2412a
sanotogii <nasaereh@gmail.com>
2023-04-13 11:23:52 -0700

    2-task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - main
+ * @nmemb: arg1
+ * @size: arg2
+ * Return: void
+ */
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = malloc(nmemb * size);
+	unsigned int i;
+	char *cptr;
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	cptr = ptr;
+	for (i = 0; i < nmemb * size; i++)
+	{
+		cptr[i] = 0;
+	}
+	return (ptr);
+}
