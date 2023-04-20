865ce332378477e0384aaedda2f8abef351c59d1
sanotogii <nasaereh@gmail.com>
2023-04-19 19:37:20 -0700

    h

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != NULL && action != NULL size > 0)
+	if (array != NULL && action != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
