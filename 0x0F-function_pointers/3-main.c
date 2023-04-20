c5e3bae0a819f18548ba0331f4f15c7f13d67043
sanotogii <nasaereh@gmail.com>
2023-04-19 21:36:21 -0700

    d

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <3-calc.h>
+#include <stdlib>
+
+/**
+ * main - main
+ * @argv: arg vector
+ * @argc: arg counter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+
+	oprt = get_op_func(argv[2])
+	if (arg != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", oprt(atoi(argv[1]), atoi(arg[3])));
+	return (0);
+
+
+}
