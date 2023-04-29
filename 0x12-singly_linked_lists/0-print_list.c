bd04f3e8bff8eefb4ba5230bf85ef785f7e00095
sanotogii <nasaereh@gmail.com>
2023-04-28 18:25:09 -0700

    0-task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print list
+ * @h: linked list
+ * Return: c
+ */
+
+size_t print_list(const list_t *h)
+{
+	int c = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
