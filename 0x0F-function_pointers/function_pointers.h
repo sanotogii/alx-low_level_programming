710a1b324e074f6f02ff0ce4e1dd794cdbd2bedc
sanotogii <121005704+sanotogii@users.noreply.github.com>
2023-05-01 13:14:03 +0200

    Delete 0x0F-function_pointers directory

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,9 +0,0 @@
-#ifndef HDR
-#define HDR
-
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
