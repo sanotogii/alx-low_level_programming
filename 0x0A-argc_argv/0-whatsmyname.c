b5a0627a8f6f196eac5121e868eab2869e9fd4b7
sanotogii <nasaereh@gmail.com>
2023-06-10 11:32:59 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)) , char *argv[])
+{
+	printf("%s",argv[0]);
+	return (0);
+}
