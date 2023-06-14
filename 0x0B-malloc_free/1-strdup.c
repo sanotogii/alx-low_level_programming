c1d0eef753795f9934e7ca6f869cb23150e39ae2
sanotogii <nasaereh@gmail.com>
2023-06-14 12:41:21 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - function create array of chars
+ * @str: arg
+ * Return: arr
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int len;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	arr = malloc(sizeof(char) * (len + 1));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		arr[i] = str[i];
+
+	return (arr);
+
+}
