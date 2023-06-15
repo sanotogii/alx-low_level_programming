7ba8de17e7d32f956a46482b159a12092df8145b
sanotogii <nasaereh@gmail.com>
2023-06-15 03:58:16 -0700

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+
+/**
+ * _realloc - main
+ * @ptr: arg1
+ * @old_size: arg2
+ * @new_size: arg3
+ * Return: void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	char *t;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+	p = malloc(new_size);
+
+	if (!p)
+		return (NULL);
+	t = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			p[i] = t[i];
+	}
+	if (new_size > old_size; i++)
+	{
+		for (i = 0; i < old_size; i++)
+			p[i] = t[i];
+	}
+	free(ptr);
+	return (p);
+}
