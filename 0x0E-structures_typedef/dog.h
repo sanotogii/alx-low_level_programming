369e05d929ad0aa24e26ccb51bdc55d25c356cb9
sanotogii <nasaereh@gmail.com>
2023-06-19 06:44:16 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - dog infos
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: dog data
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
