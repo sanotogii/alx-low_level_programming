58afcca451d1aea64126ca318f757eb51571c613
sanotogii <nasaereh@gmail.com>
2023-06-19 07:41:30 -0700

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,11 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - like an alias for struct dog
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
