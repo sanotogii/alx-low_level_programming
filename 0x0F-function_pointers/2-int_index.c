441f4000985165e42a872860914414ff87b6506a
sanotogii <nasaereh@gmail.com>
2023-06-21 04:11:44 -0700

    task-2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - main
+ * @array: array
+ * @size: size
+ * @cmp: _
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+					return (i);
+			}
+		}
+	}
+	return (-1);
+}
