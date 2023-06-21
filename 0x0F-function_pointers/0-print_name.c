ba21cf2bc506470c38d49bf56e1ac4feca0cdcc0
sanotogii <nasaereh@gmail.com>
2023-06-21 03:58:56 -0700

    task-0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - main
+ * @f: function
+ * @name: name
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
