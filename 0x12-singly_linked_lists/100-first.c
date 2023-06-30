82bcee42bcfd7900409cbd11a8ec10e6ad8f3621
sanotogii <nasaereh@gmail.com>
2023-06-30 09:17:53 -0700

    5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * befor - aa
+ * Return: void
+ */
+
+
+void __attribute__ ((constructor)) befor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
