fa8d14e0fe4c98875b8d04757a8035af34d1c531
sanotogii <nasaereh@gmail.com>
2023-06-30 09:21:43 -0700

    important files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - print list
+ * @h: linked list
+ * Return: c
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
