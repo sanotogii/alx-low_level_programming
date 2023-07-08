583eba8413f94c3942d5e89623c4b104979450cb
sanotogii <nasaereh@gmail.com>
2023-07-07 19:41:46 -0700

    0 task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - aa
+ * @b: arg
+ *
+ * Return: unsigned number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int nbr;
+	const char *p = b;
+
+	if (b)
+		return (0);
+	nbr = 0;
+
+	while (*p)
+	{
+		if (*p != '0' && *p != '1')
+			return (0);
+		nbr = (2 * nbr) + (*p - '0');
+		p++
+	}
+	return (nbr);
+
+}
