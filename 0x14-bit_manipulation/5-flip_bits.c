a0b1227d0095517aef1355f683ebf9bc76c9ac7f
sanotogii <nasaereh@gmail.com>
2023-07-07 19:57:43 -0700

    5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * flip_bits - aa
+ * @n: aa
+ * @m: aa
+ * Return: aa
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int p = n ^ m;
+	unsigned int c = 0;
+
+	while (p)
+	{
+		p &= p - 1;
+		c++
+	}
+	return (c);
+
+}
