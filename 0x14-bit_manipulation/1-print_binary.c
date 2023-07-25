9907f36f270eec1222d709fd5bd59a605f272e85
sanotogii <nasaereh@gmail.com>
2023-07-25 05:28:01 -0700

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * print_binary - unsigned to binary
+ * @n: number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int byte = 64, flag = 0;
+
+	while (byte--)
+	{
+		if (n & (1UL << byte))
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag)
+			_putchar('0');
+	}
+	if (!flag)
+		_putchar('0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,4 +4,6 @@
 #include <stdio.h>
 unsigned int binary_to_uint(const char *b);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif
