4f6c0d42e7eb51fe8d25671903846933b4dcee7a
sanotogii <nasaereh@gmail.com>
2023-07-27 07:37:32 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,9 +26,10 @@ int create_file(const char *filename, char *text_content)
 
 	}
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	file = open(filename,O_TRUNC | O_WRONLY);
 
 	if (file == -1)
+
 		return (-1);
 
 	c = write(file, text_content, len);
