a092a0072f6ab15dd226f81ff3d3c5d4d7071a8c
sanotogii <nasaereh@gmail.com>
2023-07-27 01:50:41 -0700

    task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t c;
 
-	if (filename)
+	if (filename == NULL)
 		return (0);
 
 	file = open(filename, O_RDONLY);
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(sizeof(char) * letters);
 
-	if (buffer)
+	if (buffer == NULL)
 		return (0);
 
 	c = read(file, buffer, letters);
@@ -32,6 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	write(1, buffer, c);
+
 	free(buffer);
 	close(file);
 	return (c);
