36592ad53f16ca6a0f724198b801b07dd34339b7
sanotogii <nasaereh@gmail.com>
2023-07-27 07:10:36 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,19 +11,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int file;
 	ssize_t c;
-	size_t len;
+	size_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
+		return (1);
+
+	while (*text_content != '\0')
 	{
-		while (*text_content)
-		{
-			len++;
-			text_content++;
-		}
+		len++;
+		text_content++;
+
 	}
+
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (file == -1)
