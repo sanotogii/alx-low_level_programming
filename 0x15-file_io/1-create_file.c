4daaebb6fde9007bfcc5f7e6c811835e9e3f68a5
sanotogii <nasaereh@gmail.com>
2023-07-27 06:58:27 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - function that reads a text
+ * @filename: file name
+ * @text_content: nbr of letters
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t c;
+	size_t len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (*text_content)
+		{
+			len++;
+			text_content++;
+		}
+	}
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (file == -1)
+		return (-1);
+
+	c = write(file, text_content, text_size);
+
+	if (c == -1)
+		return (-1);
+
+	close(file);
+	return (1);
+
+}
