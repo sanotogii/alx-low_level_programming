eb04a05e40dea58b5fd027f99405fa84c7b8c5c9
sanotogii <nasaereh@gmail.com>
2023-07-27 07:00:48 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	c = write(file, text_content, text_size);
+	c = write(file, text_content, len);
 
 	if (c == -1)
 		return (-1);
