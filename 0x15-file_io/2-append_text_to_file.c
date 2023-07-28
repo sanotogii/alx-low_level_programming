31ec62f5e5845f500373345fe4ceaf1e34c5fd0e
sanotogii <nasaereh@gmail.com>
2023-07-28 11:33:22 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function
+ * @filename: ..
+ * @text_content: ..
+ * Return: 1 or -1
+ */
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, s, len;
+
+	if (filename == NULL)
+		return (-1);
+	
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	if (!text_content)
+		return(1);
+
+	if (text_content != NULL)
+	{
+		while(*text_content != '\0')
+		{
+			len++;
+			text_content++;
+		}
+		len++;
+
+		s = write(file, text_content, len);
+
+		if (s == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	close(file);
+	return (1);
+}
