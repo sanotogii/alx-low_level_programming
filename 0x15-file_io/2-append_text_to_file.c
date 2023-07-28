dc2ad7b26d2f30f2297df199d1c1ac48e730d2d2
sanotogii <nasaereh@gmail.com>
2023-07-28 11:38:06 -0700

    t

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,27 +10,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, s, len;
+	int file, s, len, i = 0;
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	file = open(filename, O_WRONLY | O_APPEND);
 
 	if (file == -1)
 		return (-1);
 
 	if (!text_content)
-		return(1);
+		return (1);
 
 	if (text_content != NULL)
 	{
-		while(*text_content != '\0')
+		while (text_content[i] != '\0')
 		{
 			len++;
-			text_content++;
+			i++;
 		}
-		len++;
 
 		s = write(file, text_content, len);
 
