76e7aae24dcce552dbb06dfd8a91cfb7b48b83b1
sanotogii <nasaereh@gmail.com>
2023-07-28 15:19:24 -0700

    task adv.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, f;
+	int file, ef;
 	ssize_t c;
 	size_t len = 0;
 
@@ -17,7 +17,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
+	{
+		ef = creat(filename, 0600);
+		if (ef == -1)
+			return (-1);
 		return (-1);
+	}
 
 	while (*text_content != '\0')
 	{
@@ -31,10 +36,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (file == -1)
 	{
-		f = creat(filename, 0600);
-		if (f == -1)
-			return (-1);
-
 		return (-1);
 	}
 
