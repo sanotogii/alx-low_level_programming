51da26b1c20bd713aa265f5f92a8a8ab4ee3afff
sanotogii <nasaereh@gmail.com>
2023-07-28 14:56:29 -0700

    task adv

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+void display_elf(const char *filename)
+{
+	int i, file = open(filename, O_RDONLY);
+	ssize_t br;
+	Elf64_Ehdr elf_header;
+
+	if (file == -1)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Error: Cannot open the file");
+		exit(98);
+	}
+	br = read(file, &elf_header, sizeof(Elf64_Ehdr));
+	if (br != sizeof(Elf64_Ehdr))
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Error: Cannot read the ELF header");
+			exit(98);
+	}
+	if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
+		elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+		elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
+		elf_header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+			dprintf(STDERR_FILENO, "%s\n", "Error: Not a valid ELF file");
+			exit(98);
+	}
+	printf("Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", elf_header.e_ident[i]);
+	printf("\n");
+	printf("Class:   %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("Data:    %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little-endian" : "2's complement, big-endian");
+	printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
+	printf("OS/ABI:  %d\n", elf_header.e_ident[EI_OSABI]);
+	printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
+	printf("Type:    %d\n", elf_header.e_type);
+	printf("Entry point address: %lx\n", (unsigned long)elf_header.e_entry);
+	close(file);
+}
+
+int main(int argc, char *argv[])
+{
+	const char *filename;
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: elf_header elf_filename");
+		exit(98);
+	}
+
+	filename = argv[1];
+	display_elf(filename);
+
+	return(0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,9 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <elf.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void display_elf(const char *filename);
 #endif
