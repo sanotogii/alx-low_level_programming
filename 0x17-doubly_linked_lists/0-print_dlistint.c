6fc818d01ed6464deee778a88c6a8b5b1888c662
sanotogii <nasaereh@gmail.com>
2023-08-17 12:57:04 -0700

    task 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - display all the elements of a doubly list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
