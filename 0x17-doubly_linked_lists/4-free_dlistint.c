ee47111bc7e4733936e2f383794648cd54380e09
sanotogii <nasaereh@gmail.com>
2023-08-18 12:54:29 -0700

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	free(head);
+}
