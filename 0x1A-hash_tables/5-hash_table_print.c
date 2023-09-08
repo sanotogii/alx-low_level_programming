25045caf1c2f831de2be4085da7bdbccde55d8a6
sanotogii <nasaereh@gmail.com>
2023-09-08 16:18:04 -0400

    5-task

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,13 +7,14 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *current = ht->array[i];
+	hash_node_t *current;
 
 	if (ht == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
+		current = ht->array[i];
 		while (current != NULL)
 		{
 			printf("'%s': '%s'", current->key, current->value);
