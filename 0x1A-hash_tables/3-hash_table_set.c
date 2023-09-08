cbc478e900c85b44be0e69c1d24b8825ba4cd890
sanotogii <nasaereh@gmail.com>
2023-09-08 15:32:45 -0400

    3-task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = NULL;
 
 	new->next = ht->array[index];
 	ht->array[index] = new;
