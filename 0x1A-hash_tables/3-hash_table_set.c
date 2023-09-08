6920925b5f93096250b2ff2b85d5c98c44b2882e
sanotogii <nasaereh@gmail.com>
2023-09-08 15:26:12 -0400

    3-task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,15 +11,15 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long index = key_index((unsigned const char *)key, ht->size);
-	hash_node_t *new = malloc(sizeof(hash_node_t));
+	hash_node_t *new = malloc(sizeof(hash_node_t)), 
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 	if (new == NULL)
 		return (0);
+
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = NULL;
 
 	new->next = ht->array[index];
 	ht->array[index] = new;
