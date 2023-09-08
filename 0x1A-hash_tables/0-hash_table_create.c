9ca93c6f2cdf4e9f730db5b2b676ce1616d4c52f
sanotogii <nasaereh@gmail.com>
2023-09-08 11:32:31 -0400

    first task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -27,10 +27,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
-	{
-		free(table);
 		return (NULL);
-	}
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
