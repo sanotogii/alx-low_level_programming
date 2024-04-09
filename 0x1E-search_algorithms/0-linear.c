64c78b41c450f2a8af9571cf90c46bf141fe3448
sanotogii <nasaereh@gmail.com>
2024-04-09 06:53:17 +0000

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t index;
 
 	for (index = 0; index < size; index++)
-	{	printf("value checked array[%ld] = [%d]\n", index, array[index]);
+	{	printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 		if (array[index] == value)
 			return (index);
 	}
