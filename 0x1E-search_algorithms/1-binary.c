af6972979101222fc2f4d371f10cbfa847d39fb5
sanotogii <nasaereh@gmail.com>
2024-04-09 16:49:46 +0000

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Function: binary_search
+ * Performs a binary search on a sorted integer array 
+ * to find the index of a specific value.
+ *
+ * array: Pointer to the sorted integer array.
+ * size: Size of the array.
+ * value: The integer value to be searched for.
+ *
+ * returns: Index of the value if found, otherwise -1.
+ */
+
+
+int binary_search(int *array, size_t size, int value)
+{
+	int l = 0, r = (int)size - 1, mid = 0, i;
+
+	while (l <= r)
+	{
+		mid = (l + r) / 2;
+
+		printf("Searching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			printf("%d", array[i]);
+			if (i < r)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
+		if (value > array[mid])
+			l = mid + 1;
+		else if (value < array[mid])
+			r = mid - 1;
+		else
+			return (mid);
+	}
+
+	return (-1);
+}
+
